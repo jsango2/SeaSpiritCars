39f5a684d20c62e2569e337931a5177054201246
jsango2 <jure.sango@gmail.com>
2025-06-10 18:07:13 +0200

    first commit

--- pages/_document.js
+++ pages/_document.js
@@ -32,7 +32,14 @@ class MyDocument extends Document {
   render() {
     return (
       <Html>
-        <Head />
+        <Head>
+          <link rel="preconnect" href="https://fonts.googleapis.com" />
+          <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+          <link
+            href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100..900;1,100..900&display=swap"
+            rel="stylesheet"
+          ></link>
+        </Head>
         <body>
           <Main />
           <NextScript />
